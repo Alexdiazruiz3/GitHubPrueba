3fd50f818f07d0b0182f93f380431cb457aeffce
Alvaro <Alvaro@DESKTOP-6PLSDOE>
2018-03-04 13:02:11 +0100

    Modificado

--- GitHubPrueba/src/es/studium/GitHubPrueba/GitHubPrueba.java
+++ GitHubPrueba/src/es/studium/GitHubPrueba/GitHubPrueba.java
@@ -1,5 +1,28 @@
 package es.studium.GitHubPrueba;
 
-public class GitHubPrueba {
+public class GitHubPrueba{
 
+	String matricula ;
+	int cilindrada ;
+	public GitHubPrueba() {
+		matricula = "";
+		cilindrada = 0;
+	}
+	public GitHubPrueba(String m , int c ) {
+		matricula = m ;
+		cilindrada = c ;
+	}
+	public String getMatricula () {
+		return matricula ;
+	}
+	public int getCilindrada () {
+		return cilindrada ;
+	}
+	public void setMatricula (String m ) {
+		matricula = m ;
+	}
+	public void setCilindrada ( int c ) {
+		cilindrada = c ;
+	}
 }
+
